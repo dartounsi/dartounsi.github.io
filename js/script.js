f6fab5c8f04a5af0795fa1b4c66e345a29e969bc
Leyka <skander.kc@gmail.com>
2014-11-07 19:50:49 -0500

    Initial project import

---
+++ js/script.js
@@ -0,0 +1,128 @@
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function () {
+    
+
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 800, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+});
+
+//Google Map Skin 
+var latlng = new google.maps.LatLng(45.495410, -73.619005); 
+var myOptions = {
+    zoom: 13,
+    center: latlng,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    disableDefaultUI: true,
+    styles: [
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#acbcc9"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "color": "#f2e5d4"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#c5c6c6"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e4d7c6"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#fbfaf7"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#c5dac6"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": 33
+            }
+        ]
+    },
+    {
+        "featureType": "road"
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {},
+    {
+        "featureType": "road",
+        "stylers": [
+            {
+                "lightness": 20
+            }
+        ]
+    }
+]
+};
+
+var map = new google.maps.Map(document.getElementById('map'), myOptions);
+
+var marker = new google.maps.Marker({
+  position: latlng,
+  map: map
+});
+
+var infowindow = new google.maps.InfoWindow({
+    content: "3535 Chemin Queen Mary, Suite 410 <br/> Montréal, Québec - Canada <br/> H3V1H8"
+});
+
+infowindow.open(map,marker);
